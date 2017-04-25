c9ced21f837438920b6d931c7fe74539bcf1ed50
operfly <operfly@foxmail.com>
2017-04-25 17:06:26 +0800

    Initial commit

---
+++ distribute_scpider.py
@@ -0,0 +1,29 @@
+import time
+import requests
+import re
+import redis
+import Queue
+import threading
+
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'}
+r = redis.StrictRedis(host='192.168.1.135',port=6379,db=0)
+
+#class Mytheard(threading.Thread):
+#    """docstring for Mytheard"""
+#    def __init__(self,t_name, Queue):
+#        threading.Theard.__init__(self, name = t_name)
+#        self.data = Queue
+def upload_page_list():
+    for a in range(1,51):
+        url = 'https://www.renrenche.com/cq/ershouche/p' + str(a)
+        page_url = requests.get(url,headers = headers,timeout = 30)
+        recom = re.compile(r'data-car-id="(.*?)"',re.S)
+        car_page_url = re.findall(recom,page_url.text)
+        for b in car_page_url:
+            temp_car_page_url = 'https://www.renrenche.com/cq/car/' + str(b)
+            r.set(b,temp_car_page_url)
+        print r.get(b)
+    return 0
+print upload_page_list()
---
+++ math.py
@@ -0,0 +1,6 @@
+import math
+import requests
+
+for a in range(51):
+    url = 'https://www.renrenche.com/cq/ershouche/p' + str(a)
+    print url
